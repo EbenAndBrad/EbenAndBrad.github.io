24e8a6bb2cb2243c04fc3e392fec15fd94617a4c
EbenAndBrad <beastwick19@gmail.com>
2016-11-29 22:05:57 -0600

    asd

--- home/translate/translate.js
+++ home/translate/translate.js
@@ -7,6 +7,9 @@ console.log("\u2593\u2593\u2593  \u2593  \u2593 \u2593   \u2593 \u2593\u2593\u25
 document.getElementById("button").addEventListener("mousedown", function() {
 	var dna = document.getElementById("dna").value;
 	var mrna = '';
+	var trna = '';
+	
+	//mRNA
 	for(i = 0; i < dna.length; i++) {
 		var a = dna.substring(i, i+1);
 		if(a === 'a') {
@@ -20,4 +23,21 @@ document.getElementById("button").addEventListener("mousedown", function() {
 		}
 	}
 	document.getElementById("mrna").value = mrna;
+	
+	//tRNA
+	for(i = 0; i < mrna.length; i++) {
+		var a = mrna.substring(i, i+1);
+		if(a === 'a') {
+			trna += 'u';
+		}else if(a === 't') {
+			trna += 'a';
+		}else if(a === 'c') {
+			trna += 'g';
+		}else if(a === 'g') {
+			trna += 'c';
+		}else if(a === 'u') {
+			trna += 'a';
+		}
+	}
+	document.getElementById("trna").value = trna;
 });
